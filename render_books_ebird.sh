b668fb766002a6b23dfe263320853b43eea43146
Pratik Gupte <pratikgupte16@gmail.com>
2020-07-25 11:27:50 +0200

    udpate rnder code

--- render_books_ebird.sh
+++ render_books_ebird.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 Rscript --slave -e 'bookdown::render_book("index.rmd")'
 Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_book")'
-mv docs/ebird_wghats-supplement.tex ..
+mv docs/ebird_wghats-supplement.tex .
