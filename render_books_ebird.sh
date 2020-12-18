ec89f952356a125196a8b9ad6c362919cda16b5e
Pratik Gupte <pratikgupte16@gmail.com>
2020-12-18 16:02:06 +0100

    remove file to main

--- render_books_ebird.sh
+++ render_books_ebird.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 Rscript --slave -e 'bookdown::render_book("index.rmd")'
-Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_book")'
-mv docs/ebird_wghats-supplement.tex .
+Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_document2")'
