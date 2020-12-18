8185828e6c54e3d26b02eaaf03e3a4904bbae4e3
Pratik Gupte <pratikgupte16@gmail.com>
2020-12-18 16:03:51 +0100

    make r scripts

--- render_books_ebird.sh
+++ render_books_ebird.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-Rscript --slave -e 'bookdown::render_book("index.rmd")'
-Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_document2")'
+# render books
+Rscript --slave -e 'bookdown::render_book("index.Rmd")'
+Rscript --slave -e 'bookdown::render_book("index.Rmd", "bookdown::pdf_document2")'
+
+# make R scripts from Rmd into the R folder
+Rscript --slave -e 'lapply(list.files(pattern = "(\\d{2}_)"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
